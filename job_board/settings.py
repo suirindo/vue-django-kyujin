ee81df7a58f5194d5b682e1565b3905d0bcbc023
suirindo <lagavulin.jb.dc@gmail.com>
2022-01-24 22:21:12 +0900

    install vue

--- job_board/settings.py
+++ job_board/settings.py
@@ -123,5 +123,5 @@ STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
     'PAGE_SIZE' : 4,
-    'DEFAULT_PAGENATION_CLASS' : 'rest_framework.pagination.PageNumberPagination'
+    'DEFAULT_PAGINATION_CLASS' : 'rest_framework.pagination.PageNumberPagination'
 }
--- jobs/models.py
+++ jobs/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class JobOffer(models.Model):
-    company_Name = models.CharField(max_length = 100)
+    company_name = models.CharField(max_length = 100)
     company_email = models.EmailField()
     job_title = models.CharField(max_length = 100)
     job_description = models.TextField()
@@ -11,4 +11,4 @@ class JobOffer(models.Model):
     created_at = models.DateField(auto_now_add = True)
 
     def __str__(self):
-        return self.company_Name
+        return self.company_name
