9e01914bc5554f5f8e0150b5356692093c4c6576
suirindo <lagavulin.jb.dc@gmail.com>
2022-01-26 00:42:23 +0900

    port番号の変更

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,8 +1,9 @@
 const BundleTracker = require("webpack-bundle-tracker");
 
 module.exports = {
+    // "http://0.0.0.0:8080/"
     // "http://127.0.0.1:8080/"
-    publicPath: "http://0.0.0.0:8080/",
+    publicPath: "http://127.0.0.1:8080/",
     outputDir: "./dist/",
     transpileDependencies: ["vuetify"],
 
