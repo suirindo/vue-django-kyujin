987572970bfb539b3d2d9d5221d8cbb216e69d65
suirindo <lagavulin.jb.dc@gmail.com>
2022-01-27 01:16:18 +0900

    create

--- frontend/src/views/Job.vue
+++ frontend/src/views/Job.vue
@@ -7,6 +7,12 @@
             <p>給料: {{ job.salary }}円 </p>
             <p>都道府県: {{ job.prefectures }} </p>
             <p>市町村: {{ job.city }} </p>
+            <v-btn
+                color = "success"
+                class = "mr-2"
+                :to = "{name: 'editor', params: { id: job.id } }"
+            >編集
+            </v-btn>
         </v-container>
     </div>
 </template>
